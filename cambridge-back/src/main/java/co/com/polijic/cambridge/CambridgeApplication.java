1e5f8c280a7cc0948eb5c12d4fc6600ff8348a4b
Wilmer Moreno <morenowilmer115@gmail.com>
2025-09-28 18:29:13 -0500

    Inicio proyecto

---
+++ cambridge-back/src/main/java/co/com/polijic/cambridge/CambridgeApplication.java
@@ -0,0 +1,13 @@
+package co.com.polijic.cambridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambridgeApplication.class, args);
+	}
+
+}
---
+++ cambridge-back/src/test/java/co/com/polijic/cambridge/CambridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.polijic.cambridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
