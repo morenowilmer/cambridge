62ea41bb63f03130ca6b43f3c18fc721bf9caa21
nato9805 <nataorozcoyasno98@gmail.com>
2025-09-28 21:52:46 -0500

    se crear el componente inicial

--- cambridge-front/src/app/app.routes.ts
+++ cambridge-front/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PantallaInicialComponent } from './pantalla-inicial/pantalla-inicial.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PantallaInicialComponent },
+];
---
+++ cambridge-front/src/app/pantalla-inicial/pantalla-inicial.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantalla-inicial',
+  standalone: true,
+  imports: [],
+  templateUrl: './pantalla-inicial.component.html',
+  styleUrl: './pantalla-inicial.component.scss'
+})
+export class PantallaInicialComponent {
+
+}
